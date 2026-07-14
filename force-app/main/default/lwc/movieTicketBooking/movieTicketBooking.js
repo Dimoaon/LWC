63682da8653b2f7cd0dc575d0090828bad9e985e
Dzmitry Skavarodka <skovdimka@gmail.com>
2026-07-14 21:44:56 +0200

    feat(movieTicketBooking): add edit and delete functionality for bookings with UI updates

--- force-app/main/default/lwc/movieTicketBooking/movieTicketBooking.js
+++ force-app/main/default/lwc/movieTicketBooking/movieTicketBooking.js
@@ -29,6 +29,8 @@ export default class MovieTicketBooking extends LightningElement {
     @api freeCountLabel = null;
     @api bookedCountLabel = null;
     @api yourBookingsLabel = null;
+    @api editLabel = null;
+    @api deleteLabel = null;
     @api modalTitle = null;
     @api modalMovieLabel = null;
     @api cancelLabel = null;
@@ -37,14 +39,17 @@ export default class MovieTicketBooking extends LightningElement {
     @api lastNameLabel = null;
     @api emailLabel = null;
     @api seatLabel = null;
+    @api seatPlaceholderLabel = null;
     @api bookLabel = null;
     @api bookingSuccessLabel = null;
+    @api bookingDeletedLabel = null;
 
     maxLength = MAX_LENGTHS;
 
     @track isFirstRender = true;
     @track movies = [];
     @track selectedMovieId = null;
+    @track editingBookingId = null;
     @track form = {
         firstName: '',
         lastName: '',
@@ -89,15 +94,21 @@ export default class MovieTicketBooking extends LightningElement {
         return this.bookings.length > 0;
     }
 
+    get isEditMode() {
+        return this.editingBookingId !== null;
+    }
+
     get modalMovieInfo() {
         let movie = this.selectedMovie;
         return movie ? `${movie.title} | ${movie.showtime}` : '';
     }
 
     get seatOptions() {
-        let result = [];
+        let editing = this.isEditMode ? this.findBooking(this.editingBookingId) : null;
+        let editingSeatNumber = editing ? editing.seatNumber : null;
+        let result = [{ label: this.seatPlaceholderLabel, value: '' }];
         this.seats.forEach(seat => {
-            if (seat.isFree) {
+            if (seat.isFree || seat.number === editingSeatNumber) {
                 result.push({
                     label: `Seat ${seat.number}`,
                     value: String(seat.number)
@@ -131,7 +142,7 @@ export default class MovieTicketBooking extends LightningElement {
 
         let styles = `
             c-movie-ticket-booking lightning-input input.slds-input,
-            c-movie-ticket-booking lightning-combobox button.slds-combobox__input {
+            c-movie-ticket-booking lightning-select select.slds-select {
                 min-height: 3rem;
                 border-color: #d7d9d9;
                 align-items: center;
@@ -139,9 +150,29 @@ export default class MovieTicketBooking extends LightningElement {
 
             c-movie-ticket-booking lightning-input input.slds-input:not(:disabled):hover,
             c-movie-ticket-booking lightning-input input.slds-input:not(:disabled):focus,
-            c-movie-ticket-booking lightning-combobox input.slds-input:not(:disabled):hover,
-            c-movie-ticket-booking lightning-combobox input.slds-input:not(:disabled):focus {
+            c-movie-ticket-booking lightning-select select.slds-select:not(:disabled):hover,
+            c-movie-ticket-booking lightning-select select.slds-select:not(:disabled):focus {
                 border-color: #0064b3;
+            }
+
+            c-movie-ticket-booking .mobile-seat-picker lightning-button button {
+                width: 100%;
+                padding: 0.875rem 3rem;
+                font-size: 1.125rem;
+                font-weight: 400;
+                line-height: 1.4;
+                color: #ffffff;
+                border: 0.0625rem solid #0064b3;
+                border-radius: 0;
+                background-color: #0064b3;
+                transition: color 0.1s, border-color 0.1s, background-color 0.1s;
+            }
+
+            c-movie-ticket-booking .mobile-seat-picker lightning-button button:hover,
+            c-movie-ticket-booking .mobile-seat-picker lightning-button button:focus {
+                color: #ffffff;
+                border-color: #004d8a;
+                background-color: #004d8a;
             }
         `;
 
@@ -165,10 +196,10 @@ export default class MovieTicketBooking extends LightningElement {
     }
 
     handleSeatClick(event) {
-        let seatId = event.currentTarget.dataset.id;
-        let seat = this.findSeat(seatId);
+        let seatNumber = Number(event.currentTarget.dataset.id);
+        let seat = this.findSeatByNumber(seatNumber);
 
-        if (!seatId || !seat) {
+        if (!seat) {
             return;
         }
 
@@ -182,7 +213,9 @@ export default class MovieTicketBooking extends LightningElement {
     }
 
     handleMobileBook() {
-        if (!this.form.seat) {
+        let seatSelect = this.template.querySelector('.mobile-seat-picker lightning-select');
+
+        if (!seatSelect.reportValidity()) {
             return;
         }
 
@@ -194,6 +227,7 @@ export default class MovieTicketBooking extends LightningElement {
     }
 
     handleModalClose() {
+        this.editingBookingId = null;
         this.resetForm();
     }
 
@@ -202,10 +236,49 @@ export default class MovieTicketBooking extends LightningElement {
             return;
         }
 
-        this.bookSeat(Number(this.form.seat));
+        let number = Number(this.form.seat);
+
+        if (this.isEditMode) {
+            this.updateBooking(number);
+        } else {
+            this.bookSeat(number);
+        }
+
+        this.editingBookingId = null;
         this.resetForm();
         this.template.querySelector('c-modal').hide();
         Toast.show({ label: this.bookingSuccessLabel, variant: TOAST_VARIANT_SUCCESS }, this);
+    }
+
+    handleEditBooking(event) {
+        let bookingId = event.currentTarget.dataset.id;
+        let booking = this.findBooking(bookingId);
+
+        if (!booking) {
+            return;
+        }
+
+        this.editingBookingId = bookingId;
+        this.form = {
+            firstName: booking.firstName,
+            lastName: booking.lastName,
+            email: booking.email,
+            seat: String(booking.seatNumber)
+        };
+        this.openBookingModal();
+    }
+
+    handleRemoveBooking(event) {
+        let bookingId = event.currentTarget.dataset.id;
+        let booking = this.findBooking(bookingId);
+
+        if (!booking) {
+            return;
+        }
+
+        this.removeBooking(bookingId);
+        this.setSeatFree(booking.seatNumber);
+        Toast.show({ label: this.bookingDeletedLabel, variant: TOAST_VARIANT_SUCCESS }, this);
     }
 
     // MAIN METHODS
@@ -245,18 +318,8 @@ export default class MovieTicketBooking extends LightningElement {
         return result;
     }
 
-    findSeat(seatId) {
-        let result = null;
-        this.seats.forEach(seat => {
-            if (seat.id === seatId) {
-                result = seat;
-            }
-        });
-        return result;
-    }
-
     validateForm() {
-        let fields = [...this.template.querySelectorAll('.modal-form lightning-input, .modal-form lightning-combobox')];
+        let fields = [...this.template.querySelectorAll('.modal-form lightning-input, .modal-form lightning-select')];
         let isValid = true;
 
         fields.forEach(field => {
@@ -270,21 +333,72 @@ export default class MovieTicketBooking extends LightningElement {
     }
 
     bookSeat(number) {
-        let movie = this.selectedMovie;
+        this.setSeatBooked(number);
 
-        this.seats.forEach(seat => {
-            if (seat.number === number) {
-                seat.isFree = false;
-                seat.isBooked = true;
-            }
-        });
-
-        movie.bookings.push({
+        this.selectedMovie.bookings.push({
             id: this.generateId(),
             seatNumber: number,
-            guestName: `${this.form.firstName} ${this.form.lastName}`,
+            firstName: this.form.firstName,
+            lastName: this.form.lastName,
             email: this.form.email
         });
+    }
+
+    updateBooking(number) {
+        let booking = this.findBooking(this.editingBookingId);
+
+        if (booking.seatNumber !== number) {
+            this.setSeatFree(booking.seatNumber);
+            this.setSeatBooked(number);
+        }
+
+        booking.seatNumber = number;
+        booking.firstName = this.form.firstName;
+        booking.lastName = this.form.lastName;
+        booking.email = this.form.email;
+    }
+
+    findSeatByNumber(number) {
+        let result = null;
+        this.seats.forEach(seat => {
+            if (seat.number === number) {
+                result = seat;
+            }
+        });
+        return result;
+    }
+
+    setSeatFree(number) {
+        let seat = this.findSeatByNumber(number);
+
+        if (seat) {
+            seat.isFree = true;
+            seat.isBooked = false;
+        }
+    }
+
+    setSeatBooked(number) {
+        let seat = this.findSeatByNumber(number);
+
+        if (seat) {
+            seat.isFree = false;
+            seat.isBooked = true;
+        }
+    }
+
+    findBooking(bookingId) {
+        let result = null;
+        this.bookings.forEach(item => {
+            if (item.id === bookingId) {
+                result = item;
+            }
+        });
+        return result;
+    }
+
+    removeBooking(bookingId) {
+        let movie = this.selectedMovie;
+        movie.bookings = movie.bookings.filter(item => item.id !== bookingId);
     }
 
     resetForm() {
